5b4a690d24355cacd44d07bb7ccc6c88872e63ac
daqichen <daqichen@live.unc.edu>
2025-02-09 21:57:06 -0500

    feat(npm): #5 publish library onto npm registry

---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import horiScrollTheme from './theme';
+
+addons.setConfig({
+  theme: horiScrollTheme,
+});
---
+++ .storybook/theme.ts
@@ -0,0 +1,17 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
+
+  brandTitle: 'HoriScroll',
+  brandUrl: 'https://github.com/daqichen/hori-scroll',
+  // brandImage: 'https://www.svgrepo.com/show/297912/conveyor.svg',
+  brandTarget: '_self',
+
+  //
+  // colorPrimary: '#3A10E5',
+  // colorSecondary: '#585C6D',
+});
